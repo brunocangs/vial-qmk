60874872291095669007b135f939b5458c1ca618
Bruno Cangussu <bruno.cangussu@hotmail.com>
2024-07-21 20:13:56 -0300

    Added my own keyboard layout with vial

---
+++ keyboards/handwired/brunocangs/cosmos_1/keymaps/default/keymap.c
@@ -0,0 +1,32 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT(
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC,
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC,
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC,
+		KC_SLSH, KC_SPC, KC_SPC, KC_SPC, KC_SPC,
+		KC_Z,   KC_SPC, KC_SPC, KC_SPC, KC_SPC,
+		_______, _______, _______, KC_SPC, KC_SPC,
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC,
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC,
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC,
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC,
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC,
+		KC_SPC, KC_SPC, _______, _______, _______
+    )
+};
---
+++ keyboards/handwired/brunocangs/cosmos_1/keymaps/vial/config.h
@@ -0,0 +1,4 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0x8E, 0xE5, 0x2D, 0x1B, 0xF9, 0x48, 0xD0, 0xB5}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
